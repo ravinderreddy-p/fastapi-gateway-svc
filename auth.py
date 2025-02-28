b9fea558ba9507689607c790f3acc7fa91cbcdba
Ravi <ravinder9.reddy@gmail.com>
2025-02-28 15:38:22 +0530

    Working code with all the services

--- auth.py
+++ auth.py
@@ -1,14 +1,19 @@
 from fastapi import Depends, HTTPException, Security
 from fastapi.security import OAuth2AuthorizationCodeBearer
-from jose import jwt
+from jose import jwt, jwk
+from jose.constants import ALGORITHMS
 import httpx
 
 # issuer: http://localhost:8080/realms/fastapi-gateway
-KEYCLOAK_URL = "http://localhost:8080/realms/fastapi-gateway"
+# http://keycloak:8080/realms/fastapi-gateway/.well-known/openid-configuration
+KEYCLOAK_URL = "http://keycloak:8080/realms/fastapi-gateway"
+# KEYCLOAK_URL = "http://localhost:8080/realms/fastapi-gateway"
 
 # http://localhost:8080/realms/fastapi-gateway/protocol/openid-connect/certs
 JWKS_URL = f"{KEYCLOAK_URL}/protocol/openid-connect/certs"
-ALGORITHM = "RS256"
+ALGORITHM = ALGORITHMS.RS256
+# CLEINT_ID = "myclient"
+
 
 oauth2_scheme = OAuth2AuthorizationCodeBearer(
     authorizationUrl=f"{KEYCLOAK_URL}/protocol/openid-connect/auth",
@@ -29,9 +34,8 @@ async def verify_token(token: str = Security(oauth2_scheme)):
         )
         if not key:
             raise HTTPException(status_code=403, detail="Invalid token")
-
-        public_key = jwt.algorithms.RSAAlgorithm.from_jwk(key)
+        public_key = jwk.construct(key, ALGORITHM)
         payload = jwt.decode(token, public_key, algorithms=[ALGORITHM], audience="account")
         return payload
-    except Exception:
+    except Exception as ex:
         raise HTTPException(status_code=403, detail="Could not validate credentials")
--- gateway.py
+++ gateway.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Depends, HTTPException
+from fastapi import FastAPI, Form, Request, Depends, HTTPException
 import httpx
 from auth import verify_token
 
@@ -25,3 +25,44 @@ async def gateway(service: str, path: str, request: Request, user: dict = Depend
         )
 
     return response.json()
+
+
+# Service-3
+
+# Keycloak service - redirect
+
+# Login page - Submit - API gateway call [/login] with username and password and response with token
+
+# Create an API - for curl command inside API gateway service.
+
+@app.post("/login")
+async def login(username: str = Form(...), password: str = Form(...)):
+    try:
+        # token_url = "http://keycloak:8080/realms/fastapi-gateway/protocol/openid-connect/token"
+        token_url = "http://localhost:8080/realms/fastapi-gateway/protocol/openid-connect/token"
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                token_url,
+                data={
+                    "grant_type": "password",
+                    "client_id": "fastapi-client",
+                    "username": username,
+                    "password": password
+                    #"client_secret": "your-client-secret" #Replace with your client secret
+                },
+                headers={'Content-Type': 'application/x-www-form-urlencoded'}
+            )
+            response.raise_for_status()
+            # token_data = response.json()
+            # access_token = token_data["access_token"]
+            # return {"access_token": access_token}
+            return response.json()
+    except httpx.HTTPError as e:
+        print(f"Keycloak Authentication failed: {e}")
+        raise HTTPException(status_code=401, detail=f"Authentication failed: {e}")
+    except KeyError as e:
+        print(f"Invalid Keycloak response: Missing key {e}")
+        raise HTTPException(status_code=500, detail="Invalid Keycloak response")
+    except Exception as e:
+        print(f"Unexpected error during login: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
